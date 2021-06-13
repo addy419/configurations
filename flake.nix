0500012df10e352e53412cafadb3e9517023ce80
Aditya Sadawarte <adityasadawarte01@gmail.com>
2021-06-13 13:57:03 +0530

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -1,32 +1,23 @@
 {
+  description = "The work of a arch lunatic";
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-21.05";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager }: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager }: {
 
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules =
-        [ #({ pkgs, ... }: {
-           # boot.isContainer = true;
-
+        [ ({ pkgs, ... }: {
             # Let 'nixos-version --json' know about the Git revision
             # of this flake.
-            #system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-
-            # Network configuration.
-            #networking.useDHCP = false;
-            #networking.firewall.allowedTCPPorts = [ 80 ];
-
-            # Enable a web server.
-            #services.httpd = {
-            #  enable = true;
-            #  adminAddr = "morty@example.org";
-            #};
-          #})
+            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+          })
           ./hardware-configuration.nix
           ./modules/packages.nix
           ./configuration.nix
