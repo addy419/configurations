af02464c1f1617bea1e00ea3c02087de6dc8df3b
Aditya Sadawarte <adityasadawarte01@gmail.com>
2021-06-13 17:28:01 +0530

    too many changes

--- home.nix
+++ home.nix
@@ -1,18 +1,34 @@
-let
-  settings = import ./settings.nix;
-  home-manager = builtins.fetchGit {
-    url = "https://github.com/rycee/home-manager.git";
-    ref = "release-${settings.nixosVersion}";
-  };
+{ pkgs, ... }:
 
-in {
-  imports = [ (import "${home-manager}/nixos") ];
+{
+  home.packages = with pkgs; [
+    # Systray
+    stalonetray
+    # Preffered
+    alacritty neovim htop ripgrep
+    # Python
+    (python3.withPackages(ps: with ps; [ requests ]))
+    # Lua
+    (lua.withPackages(ps: with ps; [ lua-lsp ]))
+    # Nix
+    nixfmt
+    # Desktop
+    dmenu rofi picom
+    # K
+    libsForQt5.kdeconnect-kde
+    # QT theme
+    libsForQt5.qtstyleplugin-kvantum
+    # GTK theme
+    papirus-icon-theme xdg-desktop-portal numix-icon-theme
+    # Applets
+    cbatticon gvolicon nm-tray
+    # Applications
+    emacs neomutt mail-notification
+  ];
 
-  home-manager.users.${settings.wheelUser} = { ... }: {
-    programs.git = {
-      enable = true;
-      userName = settings.gitName;
-      userEmail = settings.gitEmail;
-    };
+  programs.git = {
+    enable = true;
+    userName = "Aditya Sadawarte";
+    userEmail = "adityasadawarte01@gmail.com";
   };
 }
---
+++ modules/options.nix
@@ -0,0 +1,14 @@
+{
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.aditya = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # Home manager
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.aditya = import ../home.nix;
+  };
+}
